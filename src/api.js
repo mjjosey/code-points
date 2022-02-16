643b9f557e1c5c5f68cca38b300b9832d57df7ff
mjjosey <mjjosey90@gmail.com>
2022-02-16 10:09:35 +0530

    First Commit

--- src/api.js
+++ src/api.js
@@ -11,34 +11,36 @@ const API = () => {
     });
   }, []);
 
-  const userData = useSelector((state) => state.users);
-  console.log(userData);
+  const userData = useSelector((state) => state?.users) || null;
+
   return (
     <>
       <h1>API</h1>
       <div className="container">
         <div className="row">
-          {userData.length > 0
-            ? userData.map((user) => (
-                <div className="col-sm-3 col-md-4 card" key={user.id}>
-                  <img
-                    className="img-thumbnail"
-                    style={{ height: "300px" }}
-                    src={user.avatar}
-                    alt="img"
-                  />
-                  <div className="card-body">
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <p>{user.email}</p>
-                    <p>{user.first_name}</p>
-                    <p>{user.last_name}</p>
-                  </div>
+          {userData ? (
+            userData.map((user) => (
+              <div className="col-sm-3 col-md-4 card" key={user.id}>
+                <img
+                  className="img-thumbnail"
+                  style={{ height: "300px" }}
+                  src={user.avatar}
+                  alt="img"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <p>{user.email}</p>
+                  <p>{user.first_name}</p>
+                  <p>{user.last_name}</p>
                 </div>
-              ))
-            : null}
+              </div>
+            ))
+          ) : (
+            <p>Something went wrong</p>
+          )}
         </div>
       </div>
     </>
